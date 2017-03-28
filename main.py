e2f12302d4663e88ed4283afd20094913303c0ab
Ian <ianw3214@gmail.com>
2017-03-27 20:47:21 -0500

    Add basic method stubs

--- main.py
+++ main.py
@@ -2,11 +2,38 @@
 The main program Python file
 """
 
-def main():
+def parseJSON(path):
+    """Parses the JSON file at the specified path
+
+    Args:
+        path(Str): A string specifying the path of the JSON file
+
+    Returns:
+        Dict: A dictionary representation of the JSON file
     """
-    The main function of the program
+    return {}
+
+def handleinput(inp):
+    """Handles the input passed from the user
+
+    Args:
+        inp(Str): The line input by the user
     """
-    print("this works")
+    # General structure of each case of user input
+    if inp == "something":
+        pass
+    # Needs a case for:
+    #   - Listing drinks
+    #   - Inputing inventory
+    #   - Adding Recipes
+    #   - Finding recipes from inventory
+
+def run():
+    """The main function of the program
+    """
+    userinput = input("HUH?: ")
+    while userinput != "exit":
+        handleinput(userinput)
 
 if __name__ == "__main__":
-    main()
+    run()
